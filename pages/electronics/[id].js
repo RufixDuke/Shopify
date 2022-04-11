3cc41771f7a520e58dff1edc7c3be6a6a3e13585
RufixDuke <rufixduke01@gmail.com>
2022-04-11 09:06:37 +0100

    nextjs-shopify

--- pages/electronics/[id].js
+++ pages/electronics/[id].js
@@ -1,6 +1,10 @@
 import Image from 'next/image'
+import {useRouter} from 'next/router';
 
 function OneElectronic({ electronic }) {
+    const router = useRouter();
+
+    const id = router.query.id;
     return (
         <>
             <div>Hiiiiiiiii</div>
@@ -44,18 +48,18 @@ export async function getServerSideProps(context) {
 //     }
 // }
 
-// export async function getStaticProps(context) {
-//     const id = context.params.electronicId;
-//     const response = await fetch(`https://fakestoreapi.com/products/category/electronics/${id}`)
-//     const data = await response.json()
-//     // console.log(data);
+export async function getStaticProps(context) {
+    const id = context.params.electronicId;
+    const response = await fetch(`https://fakestoreapi.com/products/category/electronics/${id}`)
+    const data = await response.json()
+    // console.log(data);
 
-//     return { 
-//         props: {
-//             electronic: data,
-//         }
-//     }
-// }
+    return { 
+        props: {
+            electronic: data,
+        }
+    }
+}
 
 
 

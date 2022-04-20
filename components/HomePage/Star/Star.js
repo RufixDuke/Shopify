a99e13503ea10426948bcf2b8585797a22cb4336
RufixDuke <rufixduke01@gmail.com>
2022-04-20 13:02:42 +0100

    Updated

--- components/HomePage/Star/Star.js
+++ components/HomePage/Star/Star.js
@@ -1,24 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-
-import {useRef} from 'react'
 import classes from './Star.module.css'
 
 
-function Star({rating}) {
-    const starTotal = 5;
-
-    for (let rat = 0; rat < rating.length; rat++) {
-        
-        const starPercentage = (rating / starTotal) * 100;
-        
-        const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
-        
-        document.querySelector(`.${rating} .star`).style.width = starPercentageRounded;
-        
-    }
-    console.log(typeof rating)
-
+function Star() {
     return (
         <>
             <div className={classes.starsInner}>
@@ -27,13 +10,14 @@ function Star({rating}) {
                 <span className={classes.star}>&#9734;</span>
 
                 <span className={classes.star}>&#9734;</span>
-            
+
                 <span className={classes.star}>&#9734;</span>
-            
+
                 <span className={classes.star}>&#9734;</span>
             </div>
         </>
     )
 }
+
 
 export default Star;

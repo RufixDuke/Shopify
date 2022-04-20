b7fa4dc51fed581cfb0551b141ce24d0c7f32b0d
RufixDuke <rufixduke01@gmail.com>
2022-04-20 17:16:08 +0100

    Updated

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home({ products }) {
           </div>
 
           <div>
-            <Image src={Phone} alt="Just Phone" width='100%' height={100} />
+            <Image className={styles.image} src={Phone} alt="Just Phone" width='100%' height='200px' />
             <Link href='/contact'>
               <a className={styles.contact}>Read More</a>
             </Link>

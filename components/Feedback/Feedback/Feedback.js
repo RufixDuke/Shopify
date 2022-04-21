8b35b212c6f5c7ee318ff9317e133a77eff17d51
RufixDuke <rufixduke01@gmail.com>
2022-04-21 11:57:34 +0100

    Updated

--- components/Feedback/Feedback/Feedback.js
+++ components/Feedback/Feedback/Feedback.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import classes from './Feedback.module.css'
 
-import Phone from '../../../public/rishabh-malhotra-83ypHTv6J2M-unsplash.jpg'
+import Phone from '../../../public/pexels-pixabay-459654.jpg'
 
 
 function Feedback({ feedback }) {
@@ -11,12 +11,17 @@ function Feedback({ feedback }) {
                 <div className={classes.note}>
                     <h2 className={classes.head}>Our Customer Say</h2>
                     <p className={classes.say}>{feedback.message}</p>
-                    <p>{feedback.star} star</p>
+                    <p>{feedback.star} &#11088;</p>
                     <h3 className={classes.name}>{feedback.name}</h3>
                     <p>{feedback.job}</p>
                 </div>
                 <div>
-                    <Image src={Phone} alt="Phone" width='100px' height='100%' />
+                    <Image
+                    className={classes.img} 
+                    src={Phone} 
+                    alt="Phone" 
+                    width='600px' 
+                    height='500px' />
                 </div>
             </div>
 

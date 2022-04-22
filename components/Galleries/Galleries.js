5121d8cc1314f6fac4e995be62f96ab15446f5c5
RufixDuke <rufixduke01@gmail.com>
2022-04-22 11:31:56 +0100

    Updated

--- components/Galleries/Galleries.js
+++ components/Galleries/Galleries.js
@@ -6,7 +6,7 @@ function Galleries({ Picture }) {
     return (
         <>
             <div>
-                <Image className={classes.img} src={Picture.image} alt="Just Picture" width={150} height={150} />
+                <Image className={classes.img} src={Picture.image} alt="Just Picture" width={200} height={200} />
             </div>
         </>
     )
--- components/Galleries/Gallery.js
+++ components/Galleries/Gallery.js
@@ -3,21 +3,50 @@ import Galleries from './Galleries'
 
 import classes from './Gallery.module.css'
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Autoplay } from "swiper";
+
+
 function Gallery() {
     return (
         <>
             <div className={classes.head}>
                 <h2 className={classes.header}>Visit Our Gallery</h2>
-                <div>
+                {/* <div>
                     <div className={classes.pic}>
                         {PictureObject.map((Picture) =>
                             <Galleries key={Picture.id} Picture={Picture} />
                         )}
                     </div>
 
+                </div> */}
+
+
+
+                <div className={classes.pic}>
+                    <Swiper
+                        slidesPerView={5}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false
+                        }}
+                        spaceBetween={10}
+                        loop={true}
+                        modules={[Autoplay]}
+                        className={classes.mySwiper}
+                    >
+                        {PictureObject.map((Picture) =>
+                            <SwiperSlide key={Picture.id}>
+                                <Galleries Picture={Picture} />
+                            </SwiperSlide>
+                        )}
+                    </Swiper>
                 </div>
             </div>
-
         </>
     )
 }

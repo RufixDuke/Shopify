c6e992af83e016ec811e715b41a5a6101221c978
RufixDuke <rufixduke01@gmail.com>
2022-04-29 09:15:51 +0100

    Updated

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,29 +2,64 @@ import Image from 'next/image';
 import { useSelector, useDispatch } from 'react-redux'
 import { delItem } from '../../components/redux/action/index'
 import MainNav from '../../components/Jewelery/Layout/MainNav';
+import Link from 'next/link';
 
 function Cart() {
     const state = useSelector((state) => state.handleCarts)
     const dispatch = useDispatch()
 
+    const handleClose = (item) => {
+        dispatch(delItem(item))
+    }
+
     const cartItems = (cartItem) => {
         return (
             <div key={cartItem.id}>
                 <div>
-                    <button></button>
+                    <button onClick={() => handleClose(cartItem)}>X</button>
                     <div className='row'>
                         <div className='column'>
-                            <Image src={cartItem.image} alt={cartItem.title} width={100} height={100} />
+                            <Image src={cartItem.image} alt={cartItem.title} width='180px' height='200px' />
+                        </div>
+                        <div>
+                            <h3>{cartItem.title}</h3>
+                            <p>${cartItem.price}</p>
                         </div>
                     </div>
                 </div>
             </div>
         )
     }
+
+    const emptyCart = () => {
+        return (
+            <div>
+                <div>
+                    <div className='row'>
+                        <h3>Your Cart is Empty</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const button = () => {
+        return (
+            <div>
+                <div>
+                    <Link href='/checkout'>
+                        <a className='link'>Proceed To Checkout</a>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
     return (
         <>
-        <MainNav />
+            <MainNav />
+            {state.length === 0 && emptyCart()}
             {state.length !== 0 && state.map(cartItems)}
+            {state.length !== 0 && button()}
         </>
     )
 }
